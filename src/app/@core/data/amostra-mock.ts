732f5cbca7db2ec82af3b7659c18a4e3c2d95e65
JAMG <augusto.magar@gmail.com>
2020-05-01 12:56:18 -0300

    Atualização de Concentração acessando amostra mock

---
+++ src/app/@core/data/amostra-mock.ts
@@ -0,0 +1,23 @@
+import { AmostraComposicao } from './amostra-composicao';
+
+export const Modelo: AmostraComposicao = {
+    id: 1,
+    Nome: 'TZ Eu Ag',
+    Matriz: 'TZO',
+    Dopantes: 'Eu Ag',
+
+    MatrizComp1: 'TeO2', ConcenMatrizComp1: 60,
+    MatrizComp2: 'ZnO', ConcenMatrizComp2: 40,
+    MatrizComp3: 'Nada', ConcenMatrizComp3: 0,
+    MatrizComp4: 'Nada', ConcenMatrizComp4: 0,
+
+    DopanteComp1: 'EuO2', ConcenDopanteComp1: 2,
+    DopanteComp2: 'Nada', ConcenDopanteComp2: 0,
+    DopanteComp3: 'Nada', ConcenDopanteComp3: 0,
+    DopanteComp4: 'Nada', ConcenDopanteComp4: 0,
+
+    NpComp1: 'AgNO3', ConcenNpComp1: 2,
+    NpComp2: 'Nada', ConcenNpComp2: 0,
+    NpComp3: 'Nada', ConcenNpComp3: 0,
+    NpComp4: 'Nada', ConcenNpComp4: 0,
+};
--- src/app/pages/amostra-composicao/amostra-composicao.component.ts
+++ src/app/pages/amostra-composicao/amostra-composicao.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {AmostraComposicao} from '../../@core/data/amostra-composicao';
 import { OnInit, Input } from '@angular/core';
+import { Modelo } from '../../@core/data/amostra-mock';
 
 interface CardSettings {
   title: string;
@@ -18,26 +19,10 @@ export class AmostraComposicaoComponent {
   CompMatriz = ['Nada', 'TeO2', 'ZnO', 'PbO', 'GeO2' ];
   CompDopantes = ['Nada', 'ErO2', 'YbO2', 'NdO2', 'EuO2' ];
   CompNPs = ['Nada', 'AgNO3', 'AgO2', 'AuO2', 'Si100' ];
-  modelo = new AmostraComposicao (
-    1,
-    'Vidro Teste',
-    '',
-    '',
-    this.CompMatriz[0], 0,
-    this.CompMatriz[0], 0,
-    this.CompMatriz[0], 0,
-    this.CompMatriz[0], 0,
-    this.CompDopantes[0], 0,
-    this.CompDopantes[0], 0,
-    this.CompDopantes[0], 0,
-    this.CompDopantes[0], 0,
-    this.CompNPs[0], 0,
-    this.CompNPs[0], 0,
-    this.CompNPs[0], 0,
-    this.CompNPs[0], 0,
-    );
+  
+  amostra = Modelo;
 
-  dopante_checkbox: boolean = true; // controle do botão
+  dopante_checkbox: boolean = false; // controle do botão
   nps = false; // controle do botão
   constructor() { }
 }
